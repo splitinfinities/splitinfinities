97562b607d533998401418aa57b5e9d8c23b7e5a
William Riley <mikeriley20@gmail.com>
2014-03-26 13:08:49 -0500

    Updates for window losing focus

--- js/vendor/metronome.js
+++ js/vendor/metronome.js
@@ -212,4 +212,37 @@ $(document).ready(function(){
 	$('body').hammer().on('doubletap', function(){
 		window.playAway = !window.playAway;
 	});
+
+	var hidden = "hidden";
+
+	// Standards:
+	if (hidden in document)
+		document.addEventListener("visibilitychange", onchange);
+	else if ((hidden = "mozHidden") in document)
+		document.addEventListener("mozvisibilitychange", onchange);
+	else if ((hidden = "webkitHidden") in document)
+		document.addEventListener("webkitvisibilitychange", onchange);
+	else if ((hidden = "msHidden") in document)
+		document.addEventListener("msvisibilitychange", onchange);
+	// IE 9 and lower:
+	else if ('onfocusin' in document)
+		document.onfocusin = document.onfocusout = onchange;
+	// All others:
+	else
+		window.onpageshow = window.onpagehide
+	= window.onfocus = window.onblur = onchange;
+
+	function onchange (evt) {
+		var v = 'visible', h = 'hidden',
+		evtMap = {
+			focus:v, focusin:v, pageshow:v, blur:h, focusout:h, pagehide:h
+		};
+		evt = evt || window.event;
+		if (evt.type in evtMap) {
+			play();
+		}
+		else {
+			play();
+		}
+	}
 });
