ab55e206c10760bb29a216b716f581833c62068b
William Riley <mikeriley20@gmail.com>
2014-04-29 05:30:59 -0500

    Updates to Simple

--- inspirations/index.php
+++ inspirations/index.php
@@ -1,15 +1,14 @@
 <?php
 if ($_SERVER['HTTP_X_PJAX'] == 'true'): ?>
-<div id="inspirations" class="panel" data-snare data-kick>
+<div id="inspirations" class="panel">
 	<div class="container">
-		<h1>a json string of things and people who made me</h1>
+		<h1>Inspirations</h1>
 		<p><a href="http://en.wikipedia.org/wiki/JSON" target="_blank" title="JSON" class="underline">JSON</a> is a data format, or a kind of language, that&rsquo;s used between systems to communicate a lot of information about a thing. Here, I&rsquo;m using <a href="http://en.wikipedia.org/wiki/Quantified_Self" target="_blank" title="Quantified Self" class="underline">&ldquo;quantified self&rdquo;</a> practices to capture as much of who I am as&nbsp;possible. </p>
 		<pre class="json"><code class="json"><?php include('../json.php'); ?></code></pre>
 		<h6 class="pull-right">databases store real life connections in binary</h6>
 		<p>this is in no way complete. You can&rsquo;t ever get a complete snapshot of a person, but it&rsquo;s worth trying. </p>
 	</div>
 </div>
-<script>$('pre code').each(function(i, e) {hljs.highlightBlock(e)})</script>
 <?php
 else:
 	header( 'Location: http://'.$_SERVER['HTTP_HOST'].'?trigger=inspirations' );
